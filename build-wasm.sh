5405d192bdf6df346fda2af996677969a7f04470
Steven de Salas <steven@desalasworks.com>
2022-04-18 21:43:14 +0200

    add.c

--- build-wasm.sh
+++ build-wasm.sh
@@ -2,4 +2,4 @@
 mkdir ./build || rm ./build/*.* || true
 
 docker run -v "$(pwd):/var/task" wasm-builder \
-  clang --target=wasm32 --no-standard-libraries -Wl,--export-all -Wl,--no-entry -o build/test.wasm src/test.c
+  clang --target=wasm32 --no-standard-libraries -Wl,--export-all -Wl,--no-entry -o build/add.wasm src/add.c
